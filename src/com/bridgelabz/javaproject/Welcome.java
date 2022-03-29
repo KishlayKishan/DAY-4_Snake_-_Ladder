cf46e0e45c4c1e2bd6ea8dd68994edd00a2a10e6
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-03-29 22:18:15 +0530

    Added::Welcome to Snake and Ladder Program

---
+++ src/com/bridgelabz/javaproject/Welcome.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.javaproject;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake Ladder program using oops concept");
+
+	}
+
+}
